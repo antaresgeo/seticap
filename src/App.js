fe280eb333a47bb9e44a10011b5c2c7ac639e247
minrock <lmoncarisg@gmail.com>
2019-01-15 22:15:09 -0500

    Changing editor config to 4 tab instead of 2 to set Airbnb style

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
-import React, { Component } from 'react';
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import authActions from './store/actions/auth.actions';
-import Login from './containers/Auth/Login/Login';
-import ForgotPassword from './containers/Auth/ForgotPassword/ForgotPassword';
-import CreateAccount from './containers/Auth/CreateAccount/CreateAccount';
-import HomePage from './containers/HomePage/HomePage';
-import Dashboard from './containers/Dashboard/Dashboard';
-import {connect} from 'react-redux';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import authActions from "./store/actions/auth.actions";
+import Login from "./containers/Auth/Login/Login";
+import ForgotPassword from "./containers/Auth/ForgotPassword/ForgotPassword";
+import CreateAccount from "./containers/Auth/CreateAccount/CreateAccount";
+import HomePage from "./containers/HomePage/HomePage";
+import Dashboard from "./containers/Dashboard/Dashboard";
+import { connect } from "react-redux";
+import "./App.css";
 
 class App extends Component {
-  componentDidMount(){
-    this.props.checkLogin();
-  }
-  
-  render() {
-    return (
-      <Switch>
-        <Route path="/auth/login/" component={Login}/>
-        <Route path="/auth/forgot-password/" component={ForgotPassword}/>
-        <Route path="/auth/create-account/" component={CreateAccount} />
-        { this.props.auth.token !== "" ? <Route path="/dashboard/" component={Dashboard} /> : null }
-        <Route path="/" exact component={HomePage} />
-        <Redirect to="/" />
-      </Switch>
-    );
-  }
+    componentDidMount() {
+        this.props.checkLogin();
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route path="/auth/login/" component={Login} />
+                <Route
+                    path="/auth/forgot-password/"
+                    component={ForgotPassword}
+                />
+                <Route path="/auth/create-account/" component={CreateAccount} />
+                {this.props.auth.token !== "" ? (
+                    <Route path="/dashboard/" component={Dashboard} />
+                ) : null}
+                <Route path="/" exact component={HomePage} />
+                <Redirect to="/" />
+            </Switch>
+        );
+    }
 }
 
-
-const mapStateToProps = (store) => {
-  return {
-    auth: store.auth
-  }
-}
+const mapStateToProps = store => {
+    return {
+        auth: store.auth
+    };
+};
 
 const mapDispatchToProps = dispatch => {
-  return {
-    checkLogin : () => dispatch(authActions.checkLogin())
-  }
-}
+    return {
+        checkLogin: () => dispatch(authActions.checkLogin())
+    };
+};
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(App)
+);
--- src/store/actions/auth.actions.js
+++ src/store/actions/auth.actions.js
@@ -1,73 +1,73 @@
-import { authActionTypes } from '../actions/actionTypes';
-import {Http, AuthHttp} from '../../axiosInstances';
+import { authActionTypes } from "../actions/actionTypes";
+import { Http, AuthHttp } from "../../axiosInstances";
 
 export const login = (user, password, history) => {
     return dispatch => {
         dispatch(authStart());
-        Http.post('/login/', {username: user, password: password}).then(
-            response => {
-                localStorage.setItem('token', response.data.token);
-                localStorage.setItem('user', response.data.user);
+        Http.post("/login/", { username: user, password: password })
+            .then(response => {
+                localStorage.setItem("token", response.data.token);
+                localStorage.setItem("user", response.data.user);
                 dispatch(authFinished(response.data));
-                history.push('/dashboard/')
-            }
-        ).catch((err) => {
-            dispatch(authError(err.response.data.error))
-        })
-    }
-}
+                history.push("/dashboard/");
+            })
+            .catch(err => {
+                dispatch(authError(err.response.data.error));
+            });
+    };
+};
 
 const authStart = () => {
     return {
-        type: authActionTypes.LOGIN_START,
+        type: authActionTypes.LOGIN_START
     };
-}
+};
 
-const authFinished = (auth) => {
+const authFinished = auth => {
     return {
         type: authActionTypes.LOGIN_SUCCESS,
         payload: auth
-    }
-}
+    };
+};
 
 const autoLogin = () => {
     return {
         type: authActionTypes.LOGIN_INITIAL_CHECK
-    }
-}
+    };
+};
 
 const authError = error => {
     return {
         type: authActionTypes.LOGIN_ERROR,
         payload: error
-    }
-}
+    };
+};
 
 const authCheckLogin = () => {
     return dispatch => {
         dispatch(autoLogin());
-        const token = localStorage.getItem('token');
-        if(token !== ''){
-            AuthHttp.get('/check/login/').then(({data}) => {
-                if(data.active){
-                    const user = localStorage.getItem('user');
-                    dispatch(authFinished({token, user}));
-                }else{
-                    dispatch(logout())
+        const token = localStorage.getItem("token");
+        if (token !== "") {
+            AuthHttp.get("/check/login/").then(({ data }) => {
+                if (data.active) {
+                    const user = localStorage.getItem("user");
+                    dispatch(authFinished({ token, user }));
+                } else {
+                    dispatch(logout());
                 }
-            })
+            });
         }
-    }
-}
+    };
+};
 
 const logout = () => {
     return {
         type: authActionTypes.LOGOUT
-    }
-}
+    };
+};
 
 export default {
     login: login,
     checkLogin: authCheckLogin,
     logout: logout
-}
+};
